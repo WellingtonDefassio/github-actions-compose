76a0cd52875ecff52debc625919c1bb299347b2b
WellingtonDefassio <well.defassio@gmail.com>
2024-12-30 15:56:37 -0300

    add s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    const bucket = core.getInput('bucket', { require: true })
+    const bucketRegion = core.getInput('bucket-region', { require: true })
+    const distFolder = core.getInput('dist-folder', { require: true })
+
+    github.context
+
+    // 2) upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+
     core.notice('Hello from mu custom JavaScript Action!')
 }
 
